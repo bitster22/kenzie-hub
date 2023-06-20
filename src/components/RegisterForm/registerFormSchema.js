376a329d4a9f35fd65ec14e9a953504ffbaea9e9
bitster22 <57819533+bitster22@users.noreply.github.com>
2023-06-20 18:16:53 -0300

    Pequeno erro na validação de senha corrigido

--- src/components/RegisterForm/registerFormSchema.js
+++ src/components/RegisterForm/registerFormSchema.js
@@ -9,10 +9,11 @@ export const registerFormSchema = z
     password: z
       .string()
       .nonempty("A senha é obrigatória")
-      .min(8, { message: "A senha precisa conter no mínimo 8 caracteres" })
-      .regex(/(?=.*?[a-zA-Z])/, "É necessário ao menos uma letra")
+      .regex(/(?=.*?[A-Z])/, "É necessário ao menos uma letra maiúscula")
+      .regex(/(?=.*?[a-z])/, "É necessário ao menos uma letra minúscula")
       .regex(/(\W)/, "É necessário ao menos um caracter especial")
-      .regex(/(?=.*?[0-9])/, "É necessário ao menos um número."),
+      .regex(/(?=.*?[0-9])/, "É necessário ao menos um número.")
+      .min(8, { message: "A senha precisa conter no mínimo 8 caracteres" }),
     confirm: z.string().nonempty("A confirmação de senha é obrigatória"),
     name: z.string().nonempty("O nome é obrigatório"),
     bio: z.string().nonempty("A biografia é obrigatória"),
